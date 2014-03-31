50de479b2d96cc2170d416d7580d46ed2c9f48c6
Matthew Harwood <matthhar12@gmail.com>
2014-03-31 13:44:31 -0700

    init commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('stylesheetApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
